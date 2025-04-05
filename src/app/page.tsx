b4f88dd4d955f0041e00540fbf3dc9d0989c3d31
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-04-05 16:53:29 -0300

    chore: add components in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Footer from '@/components/landing_page/Footer';
+import Header from '@/components/landing_page/Header';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Footer />
     </div>
   );
 }
