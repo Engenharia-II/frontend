6954e2b156faab49ecb85bc37fdf60f9e9c5f737
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-04-06 00:58:47 -0300

    update: add LeaningPath component in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Features from '@/components/landing_page/Features';
 import Footer from '@/components/landing_page/Footer';
 import Header from '@/components/landing_page/Header';
+import LearningPath from '@/components/landing_page/LearningPath';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Features />
+      <LearningPath />
       <Footer />
     </div>
   );
