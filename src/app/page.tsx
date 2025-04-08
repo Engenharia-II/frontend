210a1261d8dfa208708361e739b3985d6d14bc4b
Vinícius Barbosa <viniciusbarbosa0202@gmail.com>
2025-04-07 23:04:14 -0300

    Feature/8 frontend criar landing page (#9)
    * feat: create assets directory & logo png
    
    * feat: create assets directory & logo png
    
    * feat: add lib directory
    
    * update: update header
    
    * feat: create footer component
    
    * chore: add components in homepage
    
    * update: update footer
    
    * chore: add new packages
    
    * chore: add card shadcn/ui component
    
    * feat: add FeatureSession configs
    
    * update: add FeatureSession in homepage
    
    * style: change text colors
    
    * update: updating types directory location
    
    * feat: adding learning paths to landing page
    
    * update: add LeaningPath component in landing page
    
    * style: changing text color
    
    * feat: add CTA component in home page
    
    * Feat: ajust image and margins
    
    ---------
    
    Co-authored-by: Andrey Dantas <andreydantasvf@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import CallToAction from '@/components/landing_page/CallToAction';
+import Features from '@/components/landing_page/Features';
+import Footer from '@/components/landing_page/Footer';
+import Header from '@/components/landing_page/Header';
+import LearningPath from '@/components/landing_page/LearningPath';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Features />
+      <LearningPath />
+      <CallToAction />
+      <Footer />
     </div>
   );
 }
