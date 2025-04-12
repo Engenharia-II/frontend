61713638488ffd21fa2e9921175cda4f33d5ac62
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-04-12 00:07:49 -0300

    feat: add footer

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,11 @@
 import AuthLayout from '@/components/auth_page/AuthLayout';
+import Footer from '@/components/auth_page/Footer';
 
 export default function AuthPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-t from-slate-900 to-slate-800">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-t from-slate-900 to-slate-800">
       <AuthLayout />
+      <Footer />
     </div>
   );
 }
