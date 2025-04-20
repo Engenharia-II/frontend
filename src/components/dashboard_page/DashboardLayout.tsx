07ddfdd436c4087a12790cfbffd18b8888d4fe69
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-04-20 17:29:55 -0300

    style: change bg color

--- src/components/dashboard_page/DashboardLayout.tsx
+++ src/components/dashboard_page/DashboardLayout.tsx
@@ -2,7 +2,7 @@ import Header from './Header';
 
 export function DashboardLayout() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-slate-50">
       <Header />
     </div>
   );
--- src/components/dashboard_page/Header.tsx
+++ src/components/dashboard_page/Header.tsx
@@ -3,7 +3,7 @@ import UserGreeting from './UserGreeting';
 
 export default function Header() {
   return (
-    <header className="relative p-4 border-b-1 border-slate-200">
+    <header className="relative p-4 border-b-1 border-slate-200 bg-white">
       <Navbar />
       <UserGreeting name="João Silva" lastAccess="Hoje, 14:30" />
     </header>
