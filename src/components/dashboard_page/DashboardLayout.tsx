b1628c0a6858749d5aa32046b3ba845cfa431eaf
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-04-23 19:01:11 -0300

    style: idk

--- src/components/dashboard_page/DashboardLayout.tsx
+++ src/components/dashboard_page/DashboardLayout.tsx
@@ -1,14 +1,14 @@
 import Header from './Header';
 import ProgressTracker from './ProgressTracker';
+import StatsSection from './StatsSection';
 
 export function DashboardLayout() {
   return (
     <div className="min-h-screen bg-slate-50">
       <Header />
-      <main className="flex items-center justify-center">
-        <div className="w-2/3">
-          <ProgressTracker />
-        </div>
+      <main className="flex items-center justify-around">
+        <ProgressTracker />
+        <StatsSection />
       </main>
     </div>
   );
