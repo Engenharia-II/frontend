5c0d13a9f4f7db22ce1c5e42a3775e900b915d62
Andrey Dantas <andreydantasvf@gmail.com>
2025-05-11 12:33:55 -0300

    Feat: create middleware

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from 'next/server';
+
+const publicRoutes = [
+  { path: '/', whenAuthenticated: 'redirect' },
+  { path: '/auth', whenAuthenticated: 'redirect' }
+] as const;
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = '/auth';
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const publicRoute = publicRoutes.find((route) => route.path === pathname);
+  const token = request.cookies.get('token');
+  const redirectUrl = request.nextUrl.clone();
+
+  if (!token && publicRoute) {
+    return NextResponse.next();
+  }
+
+  if (!token && !publicRoute) {
+    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE;
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (token && publicRoute && publicRoute.whenAuthenticated === 'redirect') {
+    redirectUrl.pathname = '/app';
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config: MiddlewareConfig = {
+  matcher: ['/', '/auth', '/app/:path*']
+};
