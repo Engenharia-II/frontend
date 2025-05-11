736e39df1683d4eda008e1327e6c8cfb9da08271
Andrey Dantas <andreydantasvf@gmail.com>
2025-05-11 13:26:36 -0300

    Feat: add env configs

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,9 @@ const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = '/auth';
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const publicRoute = publicRoutes.find((route) => route.path === pathname);
-  const token = request.cookies.get('token');
+  const token = request.cookies.get(
+    process.env.NEXT_PUBLIC_COOKIE_NAME || 'token'
+  );
   const redirectUrl = request.nextUrl.clone();
 
   if (!token && publicRoute) {
