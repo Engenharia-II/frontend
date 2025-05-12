36fefc235201a31e0c134df50244ff101a27407d
Andrey Dantas <andreydantasvf@gmail.com>
2025-05-12 19:40:19 -0300

    Feat: get subjects progress on app

--- src/components/app_page/ProgressTracker.tsx
+++ src/components/app_page/ProgressTracker.tsx
@@ -1,53 +1,79 @@
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+'use client';
 
-const inProgressCourses = [
-  {
-    title: 'Algoritmos e Estruturas de Dados',
-    progress: 65
-  },
-  {
-    title: 'Desenvolvimento Web Full Stack',
-    progress: 42
-  },
-  {
-    title: 'Introdução à Inteligência Artificial',
-    progress: 18
-  }
-];
+import { useEffect, useState } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import { toast } from 'sonner';
+
+interface progressDataInterface {
+  subjectId: string;
+  name: string;
+  totalTopics: number;
+  completedTopics: number;
+  progress: number;
+}
 
 export default function ProgressTracker() {
-  // Isso é patético, mas não sei como fazer isso de outra forma
-  // e não quero perder tempo com isso agora
-  // O ideal seria fazer isso com um useEffect e um useState mas FODASE
-  let progresses = 0;
-  for (let i = 0; i < inProgressCourses.length; i++) {
-    progresses += inProgressCourses[i].progress;
+  const [progressData, setProgressData] = useState<
+    progressDataInterface[] | null
+  >(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // add interface for the user data
+  const fetchProgressData = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/subjects/progress`,
+        {
+          method: 'GET',
+          credentials: 'include'
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Não foi possível obter os progressos do usuário');
+      }
+      const data = (await response.json()) as progressDataInterface[];
+      const limitedData = data.slice(0, 3);
+      setProgressData(limitedData);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error
+          ? error.message
+          : 'Erro ao obter dados do usuário';
+      toast.error(errorMessage);
+      return null;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProgressData();
+  }, []);
+
+  if (isLoading || !progressData) {
+    return <span>Carregando...</span>;
   }
-  progresses /= inProgressCourses.length;
 
   return (
     <section className="w-full">
       <Card className="border-gray-200 lg:col-span-2">
         <CardHeader className="flex items-center justify-between">
           <CardTitle className="text-xl">Seu progresso</CardTitle>
-          <span className="font-semibold">
-            {progresses.toFixed(0)}% completo
-          </span>
         </CardHeader>
         <CardContent>
           <div className="space-y-4">
-            {inProgressCourses.map((course, index) => (
+            {progressData.map((subject, index) => (
               <div key={index}>
                 <div className="flex items-center justify-between gap-4 mb-2">
-                  <h4 className="text-sm font-semibold">{course.title}</h4>
+                  <h4 className="text-sm font-semibold">{subject.name}</h4>
                   <span className="text-sm text-slate-500">
-                    {course.progress}%
+                    {subject.progress}%
                   </span>
                 </div>
                 <div className="w-full bg-slate-100 h-2 rounded-full overflow-hidden">
                   <div
                     className="bg-black h-full rounded-full"
-                    style={{ width: `${course.progress}%` }}
+                    style={{ width: `${subject.progress}%` }}
                   />
                 </div>
               </div>
