bc832996a63814424e38b9d16cc2580229e236cf
Andrey Dantas <andreydantasvf@gmail.com>
2025-05-12 19:30:29 -0300

    Feat: get user data on header

--- src/components/app_page/Navbar.tsx
+++ src/components/app_page/Navbar.tsx
@@ -7,17 +7,55 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger
 } from '@/components/ui/navigation-menu';
-import { Search, Settings, User, LogOut } from 'lucide-react';
+import { Search, User, LogOut } from 'lucide-react';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import UserImage from '@/../public/assets/images/user_image.png';
 import Image from 'next/image';
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
+import { UserDataInterface } from './Header';
 
 export default function Navbar() {
   const router = useRouter();
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [userData, setUserData] = useState<UserDataInterface | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // add interface for the user data
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/users/get-by-id`,
+        {
+          method: 'GET',
+          credentials: 'include'
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Não foi possível obter os dados do usuário');
+      }
+      const data = (await response.json()) as UserDataInterface;
+      setUserData(data);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error
+          ? error.message
+          : 'Erro ao obter dados do usuário';
+      toast.error(errorMessage);
+      return null;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  // Fetch user data when the component mounts
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  if (isLoading || !userData) {
+    return <span>Carregando...</span>;
+  }
 
   function handleSubmit() {
     setIsSearchOpen(!isSearchOpen);
@@ -79,7 +117,7 @@ export default function Navbar() {
                   height={32}
                   className="rounded-full"
                 />
-                <span className="text-sm font-semibold">João Silva</span>
+                <span className="text-sm font-semibold">{userData.name}</span>
               </NavigationMenuTrigger>
               <NavigationMenuContent className="bg-white">
                 <ul className="grid w-48 gap-1 p-2">
@@ -90,15 +128,6 @@ export default function Navbar() {
                     >
                       <User className="h-5 w-5" />
                       <span>Perfil</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href={'/configs'}
-                      className="flex items-center gap-2 p-2 hover:bg-slate-100 rounded-md transition-all duration-200"
-                    >
-                      <Settings className="h-5 w-5" />
-                      <span>Configurações</span>
                     </Link>
                   </li>
                   <li>
