4071ddf2508d4b3c287d9c4d8a28b2e3fdcdfb8f
Andrey Dantas <andreydantasvf@gmail.com>
2025-05-13 23:56:07 -0300

    Feat: remove footer

--- src/components/app_page/AppLayout.tsx
+++ src/components/app_page/AppLayout.tsx
@@ -1,7 +1,6 @@
 import Header from './Header';
 import ProgressTracker from './ProgressTracker';
 import StatsSection from './StatsSection';
-import Footer from './Footer';
 import TopicList from './TopicList';
 import SubjectList from './SubjectsList';
 
@@ -9,7 +8,7 @@ export function AppLayout() {
   return (
     <div className="min-h-screen bg-slate-50">
       <Header />
-      <main className="flex flex-col">
+      <main className="flex flex-col pb-8">
         <section className="flex flex-col md:flex-row md:items-start items-stretch justify-center md:gap-x-8 gap-3 px-6 py-8">
           <div className="md:flex-[2]">
             <ProgressTracker />
@@ -25,7 +24,6 @@ export function AppLayout() {
           <TopicList />
         </section>
       </main>
-      <Footer />
     </div>
   );
 }
