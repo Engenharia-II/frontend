8057557ce91b6c179eee33edde6c256eed980c11
Andrey Dantas <andreydantasvf@gmail.com>
2025-05-16 00:08:27 -0300

    Feat: ajust DashboardHeader

--- src/components/app_page/dashboard/DashboardHeader.tsx
+++ src/components/app_page/dashboard/DashboardHeader.tsx
@@ -8,16 +8,27 @@ import {
   FaBookmark,
   FaGraduationCap,
   FaHome,
-  FaSearchPlus,
-  FaUserCircle
+  FaUserCircle,
+  FaUserEdit
 } from 'react-icons/fa';
 import { SkeletonBox } from '../../ui/skeleton-box';
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import Image from 'next/image';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger
+} from '@/components/ui/navigation-menu';
+import { LogOut } from 'lucide-react';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
 
 export default function DashboardHeader() {
   const { user, loading } = useUser();
+  const router = useRouter();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const formatLastAccess = (dateString: string) => {
@@ -25,6 +36,25 @@ export default function DashboardHeader() {
     const date = new Date(dateString);
     return formatDistanceToNow(date, { addSuffix: true, locale: ptBR });
   };
+
+  async function handleLogout() {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/sessions/logout`,
+        {
+          method: 'POST',
+          credentials: 'include'
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Erro ao sair');
+      }
+      toast.success('Logout realizado com sucesso');
+      router.push('/');
+    } catch (error) {
+      toast.error(error instanceof Error ? error.message : 'Erro ao sair');
+    }
+  }
 
   return (
     <header className="bg-white border-b border-gray-200 sticky top-0 z-30">
@@ -83,19 +113,53 @@ export default function DashboardHeader() {
               ) : (
                 <div className="flex items-center">
                   <button
-                    className="flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="flex md:hidden text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
                     onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
                   >
                     <FaUserCircle size={32} className="text-gray-600" />
                   </button>
-                  <div className="hidden md:block ml-3">
-                    <div className="text-sm font-medium text-gray-900">
-                      {user?.name}
-                    </div>
-                    <div className="text-xs text-gray-500">
-                      Último acesso {formatLastAccess(user?.lastAccess || '')}
-                    </div>
-                  </div>
+
+                  <NavigationMenu className="hidden md:block">
+                    <NavigationMenuList>
+                      <NavigationMenuItem>
+                        <NavigationMenuTrigger className="flex items-center gap-2 p-2 rounded-md transition-all duration-200 hover:cursor-pointer">
+                          <FaUserCircle size={32} className="text-gray-600" />
+                          <div className="hidden md:block ml-3">
+                            <div className="text-sm font-medium text-gray-900">
+                              {user?.name}
+                            </div>
+                            <div className="text-xs text-gray-500 flex items-center">
+                              Último acesso{' '}
+                              {formatLastAccess(user?.lastAccess || '')}
+                            </div>
+                          </div>
+                        </NavigationMenuTrigger>
+                        <NavigationMenuContent className="bg-white">
+                          <ul className="grid w-48 gap-1 p-2">
+                            <li>
+                              <Link
+                                href={'/app/profile'}
+                                className="flex items-center gap-2 p-2 hover:bg-slate-100 rounded-md transition-all duration-200"
+                              >
+                                <FaUserEdit className="h-5 w-5" />
+                                <span>Editar Perfil</span>
+                              </Link>
+                            </li>
+                            <li>
+                              <button
+                                onClick={handleLogout}
+                                type="button"
+                                className="flex w-full items-center gap-2 p-2 hover:bg-slate-100 rounded-md transition-all duration-200 cursor-pointer"
+                              >
+                                <LogOut className="h-5 w-5 text-red-500" />
+                                <span className="text-red-500">Sair</span>
+                              </button>
+                            </li>
+                          </ul>
+                        </NavigationMenuContent>
+                      </NavigationMenuItem>
+                    </NavigationMenuList>
+                  </NavigationMenu>
                 </div>
               )}
             </div>
@@ -109,8 +173,7 @@ export default function DashboardHeader() {
               <FaBars />
             </button>
           </div>
-        </div>
-
+        </div>{' '}
         {/* Mobile menu */}
         {mobileMenuOpen && (
           <div className="md:hidden border-t border-gray-200 px-2 py-3 space-y-1">
@@ -142,14 +205,22 @@ export default function DashboardHeader() {
               </div>
             </Link>
             <Link
-              href="/app/explore"
+              href="/app/profile"
               className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900"
               onClick={() => setMobileMenuOpen(false)}
             >
               <div className="flex items-center">
-                <FaSearchPlus className="mr-2" /> Explorar
+                <FaUserEdit className="mr-2" /> Editar Perfil
               </div>
             </Link>
+            <button
+              onClick={handleLogout}
+              type="button"
+              className="flex items-center gap-2 px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            >
+              <LogOut className="h-5 w-5 text-red-500" />
+              <span className="text-red-500">Sair</span>
+            </button>
           </div>
         )}
       </div>
